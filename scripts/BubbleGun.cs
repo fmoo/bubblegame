1e3aacfea23d153f9cb4414703f2bb8c21867975
Peter Ruibal <ruibalp@gmail.com>
2025-01-21 14:37:32 -0800

    Integrate bubble gun art

--- scripts/BubbleGun.cs
+++ scripts/BubbleGun.cs
@@ -8,29 +8,47 @@ public partial class BubbleGun : Node2D {
     [Export] float bubbleSpeed = 200;
     [Export] float rotationSpeed = 2;
     [Export] float trackSpeed = 20;
-    
+    [Export] AnimatedSprite2D animatedSprite;
+
+    public override void _Ready() {
+        base._Ready();
+        animatedSprite.SpeedScale = 0f;
+        animatedSprite.Play();
+    }
+
     public override void _Process(double delta) {
         base._Process(delta);
 
+        float speedScale = 0f;
+
         // If left/right arrow keys are pressed, rotate the gun
         if (Input.IsActionPressed("ui_left")) {
-            RotateGun(-1, delta);
+            if (RotateGun(-1, delta)) {
+                speedScale -= 0.6f;
+            }
         } else if (Input.IsActionPressed("ui_right")) {
-            RotateGun(1, delta);
+            if (RotateGun(1, delta)) {
+                speedScale += 0.6f;
+            }
         }
         if (Input.IsActionPressed("track_left")) {
             pathFollow.ProgressRatio = (pathFollow.ProgressRatio - (float)(delta * trackSpeed) + 1f) % 1f;
+            speedScale += 1f;
         } else if (Input.IsActionPressed("track_right")) {
             pathFollow.ProgressRatio = (pathFollow.ProgressRatio + (float)(delta * trackSpeed)) % 1f;
+            speedScale -= 1f;
         }
+        animatedSprite.SpeedScale = speedScale;
         if (Input.IsActionJustPressed("ui_select")) {
             Shoot();
         }
     }
 
-    void RotateGun(int direction, double delta) {
+    bool RotateGun(int direction, double delta) {
+        var oldRotation = Rotation;
         Rotation += direction * (float)delta * rotationSpeed;
         Rotation = Mathf.Clamp(Rotation, -Mathf.DegToRad(angleLimit), Mathf.DegToRad(angleLimit));
+        return oldRotation != Rotation;
     }
 
     void Shoot() {
