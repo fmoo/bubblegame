c2bcc3091ef1569ba66c5c91fef7819b392f110a
Peter Ruibal <ruibalp@gmail.com>
2025-02-13 19:51:17 -0800

    PauseGame.cs -> pause_game.gd, control_scheme_base.gd

---
+++ scripts/input/control_scheme_base.gd
@@ -0,0 +1,20 @@
+class_name ControlSchemeBase
+extends Node2D
+
+@export var ConflictingSchemes: Array[ControlSchemeBase] = [];
+@export var VisibleProcessMode: ProcessMode = ProcessMode.PROCESS_MODE_INHERIT;
+@export var InvisibleProcessMode: ProcessMode = ProcessMode.PROCESS_MODE_DISABLED;
+
+func _init() -> void:
+    visibility_changed.connect(_on_visibility_changed)
+
+func _on_visibility_changed() -> void:
+    if is_visible_in_tree():
+        process_mode = VisibleProcessMode
+    else:
+        process_mode = InvisibleProcessMode
+
+    if is_visible_in_tree():
+        for Scheme in ConflictingSchemes:
+            if Scheme.IsVisibleInTree():
+                Scheme.Hide();
---
+++ scripts/input/pause_game.gd
@@ -0,0 +1,13 @@
+class_name PauseGame
+extends ControlSchemeBase
+
+@export var PausePanel: Control
+@onready var bubbleGame = get_node("/root/BubbleGame")
+
+func _process(_delta: float) -> void:
+    if Input.is_action_just_pressed("toggle_pause"):
+        if not get_tree().is_paused():
+            if PausePanel != null:
+                bubbleGame.PauseWithPanel(PausePanel)
+        else:
+            bubbleGame.Unpause()
