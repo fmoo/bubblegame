3b1b8c7e0617caad0d38311cfabbb16c40404703
Peter Ruibal <ruibalp@gmail.com>
2025-02-13 23:30:01 -0800

    RenderGameTime.cs -> .gd

---
+++ scripts/ui/render_game_time.gd
@@ -0,0 +1,11 @@
+class_name RenderGameTime
+extends Label
+
+@onready var bubbleGame = get_node("/root/BubbleGame")
+
+func _ready() -> void:
+    bubbleGame.TimeElapsed.connect(_on_time_elapsed)
+
+func _on_time_elapsed(seconds: int) -> void:
+    var colon: String = ":"
+    text = str(seconds / 60) + colon + str(seconds % 60).pad_zeros(2)
