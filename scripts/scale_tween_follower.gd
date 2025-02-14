b676d68d8bcb71647fb63bc62e7fa10cb68b4762
Peter Ruibal <ruibalp@gmail.com>
2025-02-14 12:44:29 -0800

    ScaleTweenFollower.cs -> scale_tween_follower.gd

---
+++ scripts/scale_tween_follower.gd
@@ -0,0 +1,9 @@
+class_name ScaleTweenFollower
+extends Sprite2D
+
+@export var Follow: Node2D
+@export var ScaleMultiplier: float = 0.25
+
+func _process(_delta: float) -> void:
+    # Move the Scale of this sprite towards the Scale of the Follow node multiplied by ScaleMultiplier
+    scale = lerp(scale, Follow.scale * ScaleMultiplier, 0.1)
